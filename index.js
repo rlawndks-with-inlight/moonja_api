a1dd0b5e897857118390f80daeba7444c4dc6c4c
rlawndks4 <rlawndks0423@naver.com>
2023-09-15 17:25:32 +0900

    Feat: update

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ app.use((req, res, next) => {
   return next(err);
 });
 let server = undefined
-const HTTP_PORT = 8001;
+const HTTP_PORT = 8080;
 const HTTPS_PORT = 443;
 if (process.env.NODE_ENV == 'development') {
   server = http.createServer(app).listen(HTTP_PORT, function () {
